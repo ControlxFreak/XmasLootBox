c30266b59a66abe693b921b5cc82d209fd02a4c9
anthony trezza <anthony.t.trezza@gmail.com>
2022-11-28 08:46:25 -0500

    Added backup script. Just add cronjob.

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# This script will backup the data files used by the bot, everyday at 2AM (once a cronjob has been established).
+# The cron task should be:
+# 0 2 * * * PATH/TO/XmasLootBox/backup.sh >/dev/null 2>&1
+# Note: The >/dev/null 2>&1 will prevent it from sending an email everytime it is run
+#
+# Setup:
+# 1. Run: crontab -e
+# 2. Add: (crontask above)
+set -e
+
+timestamp=`date '+%Y%m%d%H%M'`
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+mkdir -p $SCRIPTPATH/backups
+
+ACCOUNTS="accounts.json"
+TS_ACCOUNTS="$timestamp-$ACCOUNTS"
+cp "$SCRIPTPATH/$ACCOUNTS" "$SCRIPTPATH/backups/$TS_ACCOUNTS"
+
+HISTORY="history.json"
+TS_HISTORY="$timestamp-$HISTORY"
+cp "$SCRIPTPATH/$HISTORY" "$SCRIPTPATH/backups/$TS_HISTORY"
+
+OWNERS="owners.json"
+TS_OWNERS="$timestamp-$OWNERS"
+cp "$SCRIPTPATH/$OWNERS" "$SCRIPTPATH/backups/$TS_OWNERS"
+
+NEXT_ID="next_id"
+TS_NEXT_ID="$timestamp-$NEXT_ID"
+cp "$SCRIPTPATH/$NEXT_ID" "$SCRIPTPATH/backups/$TS_NEXT_ID"
